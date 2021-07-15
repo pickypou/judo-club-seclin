a16f364299c6fddb052ecad297c5485b8a2b8dd9
Ludovic Spysschaert <ludo.spysschaert@orange.fr>
2021-07-15 10:36:04 +0200

    layout of the course part

---
+++ index.js
@@ -0,0 +1,23 @@
+$('#Tableau').on('click', function () {
+    if (document.getElementById('lesHoraires').style.display == "none") {
+        $('#lesHoraires').slideToggle(2000);
+        $('#Tableau').html('cacher les horaires');
+    } else {
+        $('#lesHoraires').slideToggle(2000);
+        setTimeout(function () {
+            $('#Tableau').html('Voir les horaires');
+        }, 250);
+    }
+});
+
+$('#toaffich').on('click', function () {
+    if (document.getElementById('epreuve').style.display == "none") {
+        $('#epreuve').slideToggle(2000);
+        $('#toaffich').html('Voir plus');
+    } else {
+        $('#epreuve').slideToggle(2000);
+        setTimeout(function () {
+            $('#toaffich').html('Voir moins');
+        }, 250);
+    }
+});
